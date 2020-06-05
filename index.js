a9a1a9129a9e5192bfa97b9f8b608b65b4473bbe
Paul Mejia Mendoza <mejillas_33@hotmail.com>
2020-06-06 01:52:18 +0200

    he inicializado los archivos html, css, y js y ahe agregado distintas imagens para el drumkit en carpeta imagenes , asi como la de sonidos

---
+++ index.js
@@ -0,0 +1,17 @@
+let drums = document.querySelectorAll('.drum')
+
+for(i = 0; i < drums.length ; i++){
+
+    document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+        let n = drum[i];
+        switch(n){
+            case w:
+                return console.log("w");
+                break;
+            case a:
+                return "s"
+                break;
+        }
+    })
+
+}
