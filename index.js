2d318525135ef51bcc5b3cfd22ccff6caad31342
Paul Mejia Mendoza <mejillas_33@hotmail.com>
2020-06-07 01:11:18 +0200

    terminado el proyecto con el archivo js

--- index.js
+++ index.js
@@ -2,16 +2,51 @@ let drums = document.querySelectorAll('.drum')
 
 for(i = 0; i < drums.length ; i++){
 
-    document.querySelectorAll('.drum')[i].addEventListener('click', function () {
-        let n = drum[i];
-        switch(n){
-            case w:
-                return console.log("w");
-                break;
-            case a:
-                return "s"
-                break;
-        }
+    document.querySelectorAll('.drum')[i].addEventListener('click', function (event) {
+       
+
+        let buttonInnerhtml = this.innerHTML;
+        makeSound(buttonInnerhtml)
+       
+  
     })
 
+}
+
+document.addEventListener('keydown', function (event) {
+    makeSound(event.key)
+})
+
+
+
+function makeSound(key) {
+    switch (key) {
+        case 'w':
+            let crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case 'a':
+            let kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+        case 's':
+            let snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        case 'd':
+            let tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+        case 'j':
+            let tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case 'k':
+            let tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case 'l':
+            let tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+    }
 }
